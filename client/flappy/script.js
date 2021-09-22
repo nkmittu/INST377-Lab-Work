82152f16bf7c48ab31c3a589e6932d300a3f27e7
nkmittu <73502000+nkmittu@users.noreply.github.com>
2021-09-22 14:41:00 -0400

    partial flappy commit

---
+++ client/flappy/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded' , () => {
+    const bird = document.querySelector('.bird')
+    const gameDisplay = document.querySelector('.game-container')
+    const ground = document.querySelector('.ground')
+    let birdLeft = 220
+    let birdBottom = 100
+    let gravity = 2
+
+    function startGame() {
+        birdBottom -= gravity
+        bird.style.bottom = birdBottom + 'px'
+        bird.style.left = birdLeft + 'px'
+    }
+    let timerID = setInterval(startGame, 20)
+
+    function jump() {
+        birdBottom += 50
+        bird.style.bottom = birdBottom + 'px'
+        console.log(birdBottom)
+    }
+    document.addEventListener('keyup', jump)
+})
